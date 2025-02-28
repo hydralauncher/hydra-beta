f1de9f9ab3ce45e8a9a4199c3687bbf628d625b5
Chubby Granny Chaser <hydralauncher@proton.me>
2025-02-28 17:06:11 +0000

    feat: adding icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ payload: string }>;
+}) {
+  const { payload } = await searchParams;
+
+  console.log(payload);
+
   return (
     <>
-      <Link href="/download-sources">Download Sources</Link>
-      <Link href="/profile/Znq8XqOR">Profile</Link>
+      <Link href="https://auth-staging.hydralauncher.gg?return_to=https://beta.hydralauncher.gg">
+        Login
+      </Link>
+
+      <Link href="/download-sources" shallow>
+        Download Sources
+      </Link>
+      <Link href="/profile/Znq8XqOR" shallow>
+        Profile
+      </Link>
     </>
   );
 }
