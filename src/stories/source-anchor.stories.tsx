dd92d1135d1006c5e75cb057beccc5bf18845efe
Hachi-R <drive8hachi@gmail.com>
2025-02-28 02:32:26 -0300

    feat: update source anchor mock

--- src/stories/source-anchor.stories.tsx
+++ src/stories/source-anchor.stories.tsx
@@ -9,13 +9,13 @@ export default {
   },
   argTypes: {
     title: { control: "text" },
-    url: { control: "text" },
+    href: { control: "text" },
   },
 };
 
 export const base = (args: SourceAnchorProps) => <SourceAnchor {...args} />;
 
 base.args = {
-  title: "Source",
+  title: "DavidKazumi",
   href: "/",
 };
