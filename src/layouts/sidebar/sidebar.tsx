b8ae2e324f1edd7b8f1357bae512fc2ca5724076
Hachi-R <drive8hachi@gmail.com>
2025-03-25 22:46:04 -0300

    feat: implement sidebar library with search functionality and game list display

--- src/layouts/sidebar/sidebar.tsx
+++ src/layouts/sidebar/sidebar.tsx
@@ -1,12 +1,17 @@
-import { Divider, RouteAnchor } from "@/components/common";
+import { Divider, Input, RouteAnchor } from "@/components/common";
+import { api } from "@/services/api.service";
+import { useAuthStore } from "@/stores/auth.store";
+import type { UserGame } from "@/types";
 import {
   DownloadSimple,
   Gear,
   House,
+  MagnifyingGlass,
   SquaresFour,
 } from "@phosphor-icons/react";
+import { useQuery } from "@tanstack/react-query";
 import clsx from "clsx";
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { SidebarContext, SidebarProvider } from "./sidebar-context";
 import { SidebarSlider } from "./sidebar-slider";
 import "./sidebar.scss";
@@ -70,6 +75,70 @@ function SidebarRoutes() {
   );
 }
 
+function SidebarLibrary() {
+  const { auth } = useAuthStore();
+  const { isCollapsed, setIsCollapsed } = useContext(SidebarContext);
+  const [search, setSearch] = useState("");
+
+  const searchbarRef = useRef<HTMLInputElement>(null);
+
+  const { data: gameLibrary } = useQuery({
+    queryKey: ["game-library", auth?.accessToken],
+    queryFn: () => {
+      if (!auth) return null;
+      return api.get<UserGame[]>("profile/games").json();
+    },
+    placeholderData: null,
+  });
+
+  const handleSearchbarClick = () => {
+    if (isCollapsed) {
+      setIsCollapsed(false);
+      searchbarRef.current?.focus();
+    }
+  };
+
+  // talvez seja melhor mover isso para um helper depois
+  const iconUrlBuilder = (objectId: string, iconHash: string) =>
+    `https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/${objectId}/${iconHash}.ico`;
+
+  return (
+    <div
+      className={clsx("sidebar-library", {
+        "sidebar-library--collapsed": isCollapsed,
+      })}
+    >
+      {gameLibrary && (
+        <Input
+          placeholder="Search"
+          iconLeft={<MagnifyingGlass size={24} />}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          inputSize={isCollapsed ? "icon" : "default"}
+          onClick={() => handleSearchbarClick()}
+          ref={searchbarRef as React.RefObject<HTMLInputElement>}
+        />
+      )}
+      <div className="sidebar-library__game-list">
+        {gameLibrary
+          ?.sort((a, b) => a.title.localeCompare(b.title))
+          .filter((game) =>
+            game.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((game) => (
+            <RouteAnchor
+              key={game.id}
+              href={`/shop/${game.id}`}
+              label={game.title}
+              icon={iconUrlBuilder(game.objectId, game.iconHash)}
+              collapsed={isCollapsed}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
 function SidebarDivider() {
   const { isCollapsed } = useContext(SidebarContext);
 
@@ -90,6 +159,7 @@ export function Sidebar() {
       <SidebarContainer>
         <SidebarRoutes />
         <SidebarDivider />
+        <SidebarLibrary />
       </SidebarContainer>
       <SidebarSlider />
     </SidebarProvider>
