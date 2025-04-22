9fa0c1f7345887d39f7fbcc493ceca09bd277bfa
Chubby Granny Chaser <hydralauncher@proton.me>
2025-04-22 13:17:57 +0100

    ci: adding host true

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  server: {
+    host: true,
+  },
   integrations: [react()],
   adapter: node({
     mode: "standalone",
