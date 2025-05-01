f2c5dcf4c576a961f351d3a03777b666ba27ab34
Chubby Granny Chaser <hydralauncher@proton.me>
2025-05-01 23:55:43 +0100

    feat: updating cargo fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,19 @@
 mod commands;
 
-use commands::{get_legacy_auth, delete_legacy_auth, save_password, get_password, delete_password};
+use commands::{delete_legacy_auth, delete_password, get_legacy_auth, get_password, save_password};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_log::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![get_legacy_auth, delete_legacy_auth, save_password, get_password, delete_password])
+        .invoke_handler(tauri::generate_handler![
+            get_legacy_auth,
+            delete_legacy_auth,
+            save_password,
+            get_password,
+            delete_password
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
