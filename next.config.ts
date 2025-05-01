d0b6e158d68e79d05b5fb6655be87ff59789a557
Chubby Granny Chaser <hydralauncher@proton.me>
2025-05-01 23:33:36 +0100

    feat: adding next pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://cdn.losbroxas.org/**")],
+  },
 };
 
 export default nextConfig;
