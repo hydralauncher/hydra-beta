f4fb521fb122e69135c7b2e91e622b7824d24dce
Hachi-R <drive8hachi@gmail.com>
2025-05-03 14:10:14 -0300

    feat: add gamepad button and axis type enums

--- src/types.ts
+++ src/types.ts
@@ -115,3 +115,30 @@ export interface Auth {
   refreshToken: string;
   tokenExpirationTimestamp: number;
 }
+
+export enum GamepadButtonType {
+  BUTTON_A = "buttonA",
+  BUTTON_B = "buttonB",
+  BUTTON_X = "buttonX",
+  BUTTON_Y = "buttonY",
+  LEFT_BUMPER = "leftBumper",
+  RIGHT_BUMPER = "rightBumper",
+  LEFT_TRIGGER = "leftTrigger",
+  RIGHT_TRIGGER = "rightTrigger",
+  DPAD_UP = "dpadUp",
+  DPAD_DOWN = "dpadDown",
+  DPAD_LEFT = "dpadLeft",
+  DPAD_RIGHT = "dpadRight",
+  LEFT_STICK_PRESS = "leftStickPress",
+  RIGHT_STICK_PRESS = "rightStickPress",
+  BACK = "back",
+  START = "start",
+  HOME = "home",
+}
+
+export enum GamepadAxisType {
+  LEFT_STICK_X = "leftStickX",
+  LEFT_STICK_Y = "leftStickY",
+  RIGHT_STICK_X = "rightStickX",
+  RIGHT_STICK_Y = "rightStickY",
+}
