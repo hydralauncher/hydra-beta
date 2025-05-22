7ad2a7cfb5602db774e93413a16db2ccd2abbf6c
Hachi-R <drive8hachi@gmail.com>
2025-05-22 11:27:22 -0300

    feat: remove sidebar context and slider components

--- src/layouts/sidebar/mock/sidebar-mock.ts
+++
@@ -1,8 +0,0 @@
-const mockUser = {
-  id: "1",
-  displayName: "zezinhomonstro",
-  profileImageUrl:
-    "https://cdn2.steamgriddb.com/icon_thumb/396bb038ac6ff551ed4b7be444669904.png",
-};
-
-export { mockUser };
