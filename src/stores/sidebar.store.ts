46c17ba4faea117ddffde5455ace2b39f5719fb6
Hachi-R <drive8hachi@gmail.com>
2025-05-23 14:58:50 -0300

    fix: refactor sidebar store to use persistence

--- src/stores/sidebar.store.ts
+++ src/stores/sidebar.store.ts
@@ -1,20 +1,8 @@
 import { create } from "zustand";
-import {
-  House,
-  SquaresFour,
-  DownloadSimple,
-  Gear,
-} from "@phosphor-icons/react";
-import type { Icon } from "@phosphor-icons/react";
-export interface SidebarRoute {
-  label: string;
-  href: string;
-  icon: Icon;
-}
+import { persist } from "zustand/middleware";
 
 export interface SidebarStore {
   isCollapsed: boolean;
-  routes: SidebarRoute[];
   searchTerm: string;
   isNotificationsOpen: boolean;
   setIsCollapsed: (isCollapsed: boolean) => void;
@@ -22,33 +10,20 @@ export interface SidebarStore {
   setIsNotificationsOpen: (isNotificationsOpen: boolean) => void;
 }
 
-export const useSidebarStore = create<SidebarStore>((set) => ({
-  isCollapsed: false,
-  routes: [
+export const useSidebarStore = create<SidebarStore>()(
+  persist(
+    (set) => ({
+      isCollapsed: true,
+      searchTerm: "",
+      isNotificationsOpen: false,
+      setIsCollapsed: (isCollapsed) => set({ isCollapsed }),
+      setSearchTerm: (searchTerm) => set({ searchTerm }),
+      setIsNotificationsOpen: (isNotificationsOpen) =>
+        set({ isNotificationsOpen }),
+    }),
     {
-      label: "Home",
-      href: "/",
-      icon: House,
-    },
-    {
-      label: "Catalogue",
-      href: "/catalogue",
-      icon: SquaresFour,
-    },
-    {
-      label: "Downloads",
-      href: "/downloads",
-      icon: DownloadSimple,
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-      icon: Gear,
-    },
-  ],
-  searchTerm: "",
-  isNotificationsOpen: false,
-  setIsCollapsed: (isCollapsed) => set({ isCollapsed }),
-  setSearchTerm: (searchTerm) => set({ searchTerm }),
-  setIsNotificationsOpen: (isNotificationsOpen) => set({ isNotificationsOpen }),
-}));
+      name: "sidebar-storage",
+      partialize: (state) => ({ isCollapsed: state.isCollapsed }),
+    }
+  )
+);
