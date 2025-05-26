210529d1ae8339fdb463088247a06011530d5ce7
Hachi-R <drive8hachi@gmail.com>
2025-05-26 01:27:08 -0300

    feat: add chips component

---
+++ src/components/common/chips.tsx
@@ -0,0 +1,34 @@
+import { Typography } from "./typography/typography";
+import { XIcon } from "@phosphor-icons/react";
+
+export interface ChipsProps {
+  label: string;
+  color: string;
+  icon?: React.ReactNode;
+  onRemove?: () => void;
+}
+
+export function Chips({ label, color, icon, onRemove }: Readonly<ChipsProps>) {
+  return (
+    <div className="chips">
+      <div className="chips__content">
+        {icon && <div className="chips__content__icon">{icon}</div>}
+
+        {color && (
+          <div
+            className="chips__content__color"
+            style={{ backgroundColor: color }}
+          />
+        )}
+
+        <Typography variant="body" className="chips__content__label">
+          {label}
+        </Typography>
+      </div>
+
+      <button className="chips__close-button" onClick={onRemove}>
+        <XIcon size={14} />
+      </button>
+    </div>
+  );
+}
