42f39e48bca7cfef4ffd231d10f8455a6146e5bc
Hachi-R <drive8hachi@gmail.com>
2025-06-01 04:10:59 -0300

    feat: add filter configuration

--- src/components/catalogue-page/index.ts
+++ src/components/catalogue-page/index.ts
@@ -1,7 +1,46 @@
 import Sidebar from "./sidebar";
-import FilterSection from "./filter-section";
+import Filters from "./filters";
+import Header from "./header";
+import type { FilterKey, FilterConfig } from "@/types";
+
+export const FILTER_COLORS = {
+  genres: "magenta",
+  userTags: "yellow",
+  developers: "cyan",
+  publishers: "greenlime",
+  downloadSourceFingerprints: "red",
+} as const;
+
+export const FILTER_CONFIG: Record<FilterKey, FilterConfig> = {
+  genres: {
+    label: "Genres",
+    color: FILTER_COLORS.genres,
+    isObject: false,
+  },
+  userTags: {
+    label: "User Tags",
+    color: FILTER_COLORS.userTags,
+    isObject: true,
+  },
+  downloadSourceFingerprints: {
+    label: "Download Sources",
+    color: FILTER_COLORS.downloadSourceFingerprints,
+    isObject: true,
+  },
+  developers: {
+    label: "Developers",
+    color: FILTER_COLORS.developers,
+    isObject: false,
+  },
+  publishers: {
+    label: "Publishers",
+    color: FILTER_COLORS.publishers,
+    isObject: false,
+  },
+} as const;
 
 export const Catalogue = {
   Sidebar,
-  FilterSection,
+  Filters,
+  Header,
 };
