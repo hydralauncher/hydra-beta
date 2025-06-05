c76cfdb82bbf2bf078f1f5e423b83f4ce16c80fc
Hachi-R <drive8hachi@gmail.com>
2025-06-04 22:59:53 -0300

    refactor: update catalogue component props

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -1,23 +1,26 @@
 import { Catalogue } from "../components/catalogue-page";
 import { useCatalogueData } from "../hooks";
-import { FILTERS } from "../components/catalogue-page/filters.config";
 
 export default function CataloguePage() {
-  const { form, catalogueData, search } = useCatalogueData();
-  const { control } = form;
+  const { values, updateSearchParams, catalogueData, search } =
+    useCatalogueData();
 
   if (!catalogueData) return null;
 
   return (
     <div className="catalogue-container">
-      <Catalogue.Header form={form} />
+      <Catalogue.Header
+        values={values}
+        updateSearchParams={updateSearchParams}
+        catalogueData={catalogueData}
+      />
 
       <div className="catalogue-content">
         <Catalogue.Grid search={search} />
         <Catalogue.Sidebar
           catalogueData={catalogueData}
-          control={control}
-          filterConfig={FILTERS}
+          values={values}
+          updateSearchParams={updateSearchParams}
         />
       </div>
     </div>
