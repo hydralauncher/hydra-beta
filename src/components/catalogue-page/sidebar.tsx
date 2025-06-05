dc8b0d4dc48233588552833a222f5c8b427ca468
Hachi-R <drive8hachi@gmail.com>
2025-06-04 22:57:38 -0300

    refactor: streamline sidebar component

--- src/components/catalogue-page/sidebar.tsx
+++ src/components/catalogue-page/sidebar.tsx
@@ -1,60 +1,47 @@
-import { Control, FieldValues } from "react-hook-form";
 import { useState } from "react";
 import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 import { Input } from "@/components";
 import { Accordion } from "@/components/common/accordion";
 import { ColorDot } from "@/components/common/chip";
 import FilterSection from "./filters-list";
-import { FilterConfig, type FilterKey } from "./filters.config";
-import { CatalogueData } from "@/hooks/use-catalogue.hook";
-
-const useLocale = () => "en" as const;
+import { CatalogueData, SearchGamesFormValues, FilterType } from "@/hooks";
 
 interface SidebarProps {
   catalogueData: CatalogueData;
-  control: Control<FieldValues>;
-  filterConfig: Record<FilterKey, FilterConfig>;
+  values: SearchGamesFormValues;
+  updateSearchParams: (newValues: Partial<SearchGamesFormValues>) => void;
 }
 
 export default function Sidebar({
   catalogueData,
-  control,
-  filterConfig,
-}: SidebarProps) {
-  const locale = useLocale();
-  const initialOpenState = Object.fromEntries(
-    Object.keys(filterConfig).map((key) => [key, true])
-  ) as Record<FilterKey, boolean>;
+  values,
+  updateSearchParams,
+}: Readonly<SidebarProps>) {
+  const [filtersSearchTerms, setFiltersSearchTerms] = useState<
+    Record<FilterType, string>
+  >({
+    [FilterType.GENRES]: "",
+    [FilterType.TAGS]: "",
+    [FilterType.DEVELOPERS]: "",
+    [FilterType.PUBLISHERS]: "",
+  });
 
-  const initialSearchState = Object.fromEntries(
-    Object.keys(filterConfig).map((key) => [key, ""])
-  ) as Record<FilterKey, string>;
-
-  const [openedFilters, setOpenedFilters] = useState(initialOpenState);
-  const [filtersSearchTerms, setFiltersSearchTerms] =
-    useState(initialSearchState);
+  const setFilterSearchTerm = (key: FilterType, term: string) => {
+    setFiltersSearchTerms((prev) => ({ ...prev, [key]: term }));
+  };
 
   if (!catalogueData) return null;
 
-  const filterData: Record<FilterKey, string[] | Record<string, number>> = {
-    genres: catalogueData.genres[locale],
-    tags: catalogueData.tags[locale],
-    developers: catalogueData.developers,
-    publishers: catalogueData.publishers,
-  };
-
-  const setOpenedFilter = (key: FilterKey, isOpen: boolean) => {
-    setOpenedFilters((prev) => ({ ...prev, [key]: isOpen }));
-  };
-
-  const setFilterSearchTerm = (key: FilterKey, term: string) => {
-    setFiltersSearchTerms((prev) => ({ ...prev, [key]: term }));
+  const filterData: Record<FilterType, string[] | Record<string, number>> = {
+    [FilterType.GENRES]: catalogueData[FilterType.GENRES].data,
+    [FilterType.TAGS]: catalogueData[FilterType.TAGS].data,
+    [FilterType.DEVELOPERS]: catalogueData[FilterType.DEVELOPERS].data,
+    [FilterType.PUBLISHERS]: catalogueData[FilterType.PUBLISHERS].data,
   };
 
   return (
     <div className="catalogue__sidebar">
-      {(Object.keys(filterConfig) as FilterKey[]).map((filterKey) => {
-        const config = filterConfig[filterKey];
+      {Object.values(FilterType).map((filterKey) => {
         const data = filterData[filterKey];
         const length = Array.isArray(data)
           ? data.length
@@ -63,25 +50,27 @@ export default function Sidebar({
         return (
           <Accordion
             key={filterKey}
-            title={config.label}
-            icon={<ColorDot color={config.color} />}
-            open={openedFilters[filterKey] ?? true}
+            open={true}
             hint={`${length} Available`}
-            onOpenChange={(isOpen) => setOpenedFilter(filterKey, isOpen)}
+            title={catalogueData[filterKey].label}
+            icon={<ColorDot color={catalogueData[filterKey].color} />}
           >
             <div className="catalogue__sidebar__filter__content">
               <Input
-                placeholder={`Search ${config.label}`}
+                type="text"
+                placeholder={`Search ${catalogueData[filterKey].label.toLowerCase()}`}
                 iconLeft={<MagnifyingGlassIcon size={24} />}
-                value={filtersSearchTerms[filterKey] || ""}
+                value={filtersSearchTerms[filterKey] ?? ""}
                 onChange={(e) => setFilterSearchTerm(filterKey, e.target.value)}
+                autoComplete="off"
               />
 
               <FilterSection
                 name={filterKey}
                 listData={data}
-                control={control}
-                searchTerm={filtersSearchTerms[filterKey] || ""}
+                values={values}
+                updateSearchParams={updateSearchParams}
+                searchTerm={filtersSearchTerms[filterKey] ?? ""}
               />
             </div>
           </Accordion>
